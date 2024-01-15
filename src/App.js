6400b61c7b70748d4ac6818b3275892831eecf31
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-01-15 17:23:45 +0500

    axdcs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Linkconditions from "./Linkconditions.js";
 
 function App() {
+  useEffect(() => {
+    function checker() {
+      console.log(process.env.REACT_APP_API_KEY);
+    }
+    checker();
+  }, []);
+
   return (
     <>
       <Linkconditions />
